ec79f044d7ba4ab80f6cc71dc16c0adb7e299731
Jonatas Santos Da Silva <66139215+Jonattaz@users.noreply.github.com>
2022-11-24 00:56:09 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#Exemplo Final do Curso de Extensão Ânima Python Básico - Orientado a objetos e acessando banco de dados!!!
+#Importar arquivo Pessoa.py
+from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
+
+#Exemplo de uso
+jonatas = Pessoa(1, "Jonatas Santos")
+print(jonatas.nome)
+
+#Chamar objeto de banco de dados
+db = Database()
+
+#Instância o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = PessoaDAO.getAll()
+for pessoa in pessoas:
+  print(pessoa)
